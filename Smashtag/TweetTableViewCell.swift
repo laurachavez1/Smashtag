a45542a92ae0f77eb759a9dcf36ca4f67b5e50b2
Laura Chavez <lachavez@LC-Mac.local>
2016-12-21 18:33:00 -0800

    Search and view tweets/ Initial commit

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Laura Chavez.
+//  Copyright © 2016 Laura Chavez. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    var tweet: Tweet?{
+        didSet{
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var hashtagColor = UIColor.purpleColor()
+    var urlColor = UIColor.blueColor()
+    var userMentionsColor = UIColor.orangeColor()
+    
+    func updateUI(){
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        //tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet {
+            var text = tweet.text
+            for _ in tweet.media {
+                text += " 📷"
+            }
+            
+            let attributedText = NSMutableAttributedString(string: text)
+            attributedText.changeKeywordsColor(tweet.hashtags, color: hashtagColor)
+            attributedText.changeKeywordsColor(tweet.urls, color: urlColor)
+            attributedText.changeKeywordsColor(tweet.userMentions, color: userMentionsColor)
+            attributedText.changeKeywordsColor(tweet.mediaMentions, color: urlColor) 
+            tweetTextLabel?.attributedText = attributedText
+            
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) {
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+//            let formatter = NSDateFormatter()
+//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+//            } else {
+//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+//            }
+//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+    }
+}
+
+// MARK: - Extensions
+private extension NSMutableAttributedString {
+    func changeKeywordsColor(keywords: [Tweet.IndexedKeyword], color: UIColor) {
+        for keyword in keywords {
+            addAttribute(NSForegroundColorAttributeName, value: color, range: keyword.nsrange)
+        }
+    }
+}
---
+++ Smashtag/TweetTableViewController.swift
@@ -0,0 +1,166 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by Laura Chavez.
+//  Copyright © 2016 Laura Chavez. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    var tweets = [[Tweet]]()
+    var searchText: String? = "#stanford" {
+        didSet{
+            lastSuccessfulRequest = nil
+            searchTextField?.text = searchText
+            tweets.removeAll()
+            tableView.reloadData()
+            refresh()
+        }
+    }
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        refresh()
+    }
+    
+    
+    func refresh(){
+        if refreshControl != nil {
+            refreshControl?.beginRefreshing()
+        }
+        refresh(refreshControl)
+    }
+    
+    var lastSuccessfulRequest: TwitterRequest?
+    var nextRequestToAttempt: TwitterRequest? {
+        if lastSuccessfulRequest == nil {
+            if searchText != nil {
+                return TwitterRequest(search: searchText!, count: 100)
+            } else {
+                return nil
+            }
+        } else {
+            return lastSuccessfulRequest!.requestForNewer
+        }
+    }
+    
+    @IBAction func refresh(sender: UIRefreshControl?) {
+        if searchText != nil {
+            if let request = nextRequestToAttempt {
+                //fetchtweets is async API therefore must re-dispatch main queue upon return
+                request.fetchTweets{ (newTweets) -> Void in
+                    dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                        if newTweets.count > 0 {
+                            self.lastSuccessfulRequest = request
+                            self.tweets.insert(newTweets, atIndex: 0)
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
+                print("requested")
+            }
+        }
+        sender?.endRefreshing()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var searchTextField: UITextField! {
+        didSet{
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == searchTextField{
+            textField.resignFirstResponder()
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tweets.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print ("ret tableView \(tweets[section].count)")
+        return tweets[section].count
+    }
+    
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "Tweet"
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> TweetTableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as! TweetTableViewCell
+        
+        cell.tweet = tweets[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
